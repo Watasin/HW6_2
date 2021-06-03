5f24f9d794d39412e47fbe0815b9d2f705cc6e15
LAPTOP-DI30GGAL\iiisinnnn <watasin.kongkhun@mail.kmutt.ac.th>
2021-06-04 01:15:47 +0700

    63120501016_HW6-2

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        int hW1, hW2, midterm, final, x;
+
+        hW1 = (int)readValue();
+        hW2 = (int)readValue();
+        midterm = (int)readValue();
+        final = (int)readValue();
+        x = hW1 + hW2 + midterm + final;
+        Console.WriteLine(x);
+        if (x > 79)
+        {
+            Console.WriteLine(" Grade A ");
+        }
+        else
+        {
+            if (x > 69)
+            {
+                Console.WriteLine(" Grade B ");
+            }
+            else
+            {
+                if (x > 59)
+                {
+                    Console.WriteLine(" Grade C ");
+                }
+                else
+                {
+                    if (x > 49)
+                    {
+                        Console.WriteLine(" Grade D ");
+                    }
+                    else
+                    {
+                        if (x < 49)
+                        {
+                            Console.WriteLine(" Grade F ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+    private static double readValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
